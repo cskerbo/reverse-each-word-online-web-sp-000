b1c1aa9dbff4e885446a7dafcd94f00277125221
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-16 21:09:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
-def reverse_each_word(sentence)
-words = [sentence.split]
-words.reverse_each {|word| puts word}
-
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
